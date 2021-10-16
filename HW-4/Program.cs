703508c718811df4a92812027a72aae574ceb7ea
Egor Smolyakov <smolyakov.en@gmail.com>
2021-10-16 19:06:10 +0300

    HW-4

--- HW-4/Program.cs
+++ HW-4/Program.cs
@@ -10,7 +10,43 @@ namespace HW_4
     {
         static void Main()
         {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                {"four",4},
+                {"two",2},
+                {"one",1},
+                {"three",3},
+            };
+            var d = dict.OrderBy(pair => pair.Value);
+            foreach (var pair in d)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
 
+
+            Console.WriteLine();
+            List<int> list = new List<int>();
+            int[] arr = new int[] { 1, 5, 34, 3, 87, 3, 45, 2, 34, 1, 1, 34 };
+            list.AddRange(arr);
+
+            var list2 = list.GroupBy(e => e);
+
+            var list3 = from item in list
+                        group item by item into gr
+                        select new { gr.Key, Count = gr.Count() };
+
+            foreach (var item in list2)
+            {
+                Console.WriteLine($"Элемент \"{item.Key}\" встречается {item.Count()} раз");
+            }
+            Console.WriteLine();
+
+            foreach (var item in list3)
+            {
+                Console.WriteLine($"Элемент \"{item.Key}\" встречается {item.Count} раз");
+            }           
+
+            Console.ReadLine();
         }
     }
 }
